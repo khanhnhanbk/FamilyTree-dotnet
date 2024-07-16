9700ae45d6ddb36c4dcba760ed96ffb74635f196
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-07-16 09:20:22 +0700

    Add project files.

---
+++ FamilyTree/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using FamilyTree.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyTree.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<People> Peoples { get; set; }
+    }
+}
---
+++ FamilyTree/Entities/People.cs
@@ -0,0 +1,10 @@
+﻿namespace FamilyTree.Entities
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
